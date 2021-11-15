b222dd45f15795156f2221dd60c9c63d1e328644
thiago-dev89 <thiago.developer30@gmail.com>
2021-11-15 19:45:26 -0300

    Exercício 3.1 do Capítulo 3 - Listas

---
+++ capitulo_03/ex3_1.py
@@ -0,0 +1,10 @@
+"""
+3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista chamada names.
+      Exiba o nome de cada pessoa acessando cada elemento da lista, um de cada vez.
+"""
+nomes = ["Thiago", "Bernardo", "Tônia", "Paulo"]
+print('*{:^40}*'.format('Listas de amigos'))
+print(nomes[0].title())
+print(nomes[1].title())
+print(nomes[2].title())
+print(nomes[3].title())
