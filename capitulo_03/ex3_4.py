2cb06b138df9e2d0853d6d1aec4bb07e832deb81
thiago-dev89 <thiago.developer30@gmail.com>
2021-11-16 01:18:21 -0300

    Exercício 3.4 do Capítulo 3 - Listas

---
+++ capitulo_03/ex3_4.py
@@ -0,0 +1,11 @@
+'''
+    3.4 – Lista de convidados: Se pudesse convidar alguém, vivo ou morto, para o
+          jantar, quem você convidaria? Crie uma lista que inclua pelo menos três pessoas
+          que você gostaria de convidar para jantar. Em seguida, utilize sua lista para
+          exibir uma mensagem para cada pessoa, convidando-a para jantar.
+'''
+convidados = ['thiago', 'bernardo', 'tônia']
+
+print(f'{convidados[0].title()}, você está convidado para o nosso jantar.')
+print(f'{convidados[1].title()}, você está convidado para o nosso jantar.')
+print(f'{convidados[2].title()}, você está convidado para o nosso jantar.')
