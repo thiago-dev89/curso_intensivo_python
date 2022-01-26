dd8a484795f4aedbf70b45470fb08aa5c4f731e8
thiago-dev89 <thiago.developer30@gmail.com>
2022-01-26 17:30:11 -0300

    Exercicio 4.6 - Números ímpares

---
+++ capitulo_04/ex4_6.py
@@ -0,0 +1,8 @@
+'''
+4.6 – Números ímpares: Use o terceiro argumento da função range() para criar
+uma lista de números ímpares de 1 a 20. Utilize um laço for para exibir todos
+os números.
+'''
+
+for i in range(1, 21, 2):
+    print(i)
