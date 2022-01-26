1fd6eba57057148dbc04044d37969710462793de
thiago-dev89 <thiago.developer30@gmail.com>
2022-01-26 17:38:48 -0300

    Exercicio 4.7 - Três

---
+++ capitulo_04/ex4_7.py
@@ -0,0 +1,12 @@
+'''
+4.7– Três: Crie uma lista de múltiplos de 3, de 3 a 30. Use um laço for para
+exibir os números de sua lista.
+'''
+
+multiplos_tres = [num for num in range(3, 31, 3)]
+
+for multiplos in multiplos_tres:
+    print(multiplos)
+
+
+
