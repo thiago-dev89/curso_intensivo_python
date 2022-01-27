f7da6729fb2253b4cd5eb720191b91740ba6fa5b
thiago-dev89 <thiago.developer30@gmail.com>
2022-01-27 18:26:33 -0300

    Exercicio 5.3 - Cores de alienígenas #1

---
+++ capitulo_05/ex5_3.py
@@ -0,0 +1,26 @@
+'''
+5.3 – Cores de alienígenas #1: Suponha que um alienígena acabou de ser
+atingido em um jogo. Crie uma variável chamada alien_color e atribua-lhe um
+valor igual a 'green', 'yellow' ou 'red'.
+
+• Escreva uma instrução if para testar se a cor do alienígena é verde. Se for,
+mostre uma mensagem informando que o jogador acabou de ganhar cinco
+pontos.
+
+• Escreva uma versão desse programa em que o teste if passe e outro em que
+ele falhe. (A versão que falha não terá nenhuma saída.)
+'''
+
+'''
+#Teste condicional True - Variável alien_color = green
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Você acaba de ganhar 5 pontos!!')
+'''
+
+#Tste condicional false - Variável alien_color = red
+alien_color = 'red'
+if alien_color == 'green':
+    print('Você acaba de ganhar 5 pontos!!')
+
