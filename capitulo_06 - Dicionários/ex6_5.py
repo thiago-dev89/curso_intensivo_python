6ac98a2526d91c1c096f5dee92f6c2a68bf6204e
thiago-dev89 <thiago.developer30@gmail.com>
2022-01-28 18:51:40 -0300

    Exercicio 6.5 - Rios

---
+++ capitulo_06 - Dicionários/ex6_5.py
@@ -0,0 +1,8 @@
+'''
+6.5 – Rios: Crie um dicionário que contenha três rios importantes e o país que
+cada rio corta. Um par chave-valor poderia ser 'nilo': 'egito'.
+• Use um laço para exibir uma frase sobre cada rio, por exemplo, O Nilo corre
+pelo Egito.
+• Use um laço para exibir o nome de cada rio incluído no dicionário.
+• Use um laço para exibir o nome de cada país incluído no dicionário.
+'''
